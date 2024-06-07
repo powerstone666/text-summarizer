d4b19a9858f3a3f2d962117e11a33e39596c35cb
imran pasha <imranpasha8225@gmail.com>
2024-06-07 15:51:59 +0530

    fixed api added loading

--- src/text.jsx
+++ src/text.jsx
@@ -4,13 +4,14 @@ function Text() {
    const [text, setText] = useState("");
    const [output, setOutput] = useState("");
    const [textColor, setTextColor] = useState("green");
+   const [loading,setLoading]=useState(false)
    const changing = (e) => {
       setText(e.target.value);
    };
    useEffect(() => {
-    if (text.length > 200) {
+    if (text.length < 200 || text.length>2500 ) {
       setTextColor("red");
-    } else {
+    } else{
       setTextColor("green");
     }
   }, [text]);
@@ -19,8 +20,9 @@ const clear = () => {
    setText("");
 }
    const summarizing = async () => {
+      setLoading(true)
     try{
-        if(text.length>=200){
+        if(text.length>=200 && text.length<=2500){
       const response = await fetch(
          "https://api-inference.huggingface.co/models/facebook/bart-large-cnn",
          {
@@ -30,13 +32,18 @@ const clear = () => {
          }
       );
       const result = await response.json();
-      
       setOutput(result[0].summary_text);
-      console.log(output);
+      setLoading(false)
+  
+    }
+    else if(text.length<200){
+        alert("Please enter atleast 200 words");
+        setLoading(false)
+    
     }
     else{
-        alert("Please enter atleast 200 words");
-    
+      alert("maximum length exceeded")
+      setLoading(false)
     }
     }
     catch(e)
@@ -52,8 +59,8 @@ const clear = () => {
          </nav>
          <p className="p-4 f text-xl text-center mt-8">Welcome to the AI Text Summarizer App! This app leverages the power of Artificial Intelligence APIs to provide concise summaries of long texts. Whether you have a lengthy article, research paper, or any other text document that you want to summarize quickly, our app can assist you.</p>
          <p className="p-4 f text-xl text-center mt-12">Simply paste your text into the text area below and click the "Submit" button.</p>
-         <div class="h-1/2 w-full flex justify-center">
-         <div class="w-4/5 flex  m-5 mt-10 ">
+         <div className="h-1/2 w-full flex justify-center">
+         <div className="w-4/5 flex  m-5 mt-10 ">
             <div className="bg-gray-50 h-full w-1/2 mr-5 rounded border-2 border-black">
                <textarea
                   type="text"
@@ -62,7 +69,7 @@ const clear = () => {
                   onChange={(e) => changing(e)} value={text}
                />
               <span className="text-xl justify-end flex m-8 mr-10 text-blue-900" >
-               <h1 style={{ color: textColor }}>{text.length}/200</h1>
+               <h1 style={{ color: textColor }}>{text.length}/2500</h1>
                </span>
             </div>
             <div className="bg-white h-full w-1/2 rounded border-2 border-black">
@@ -71,8 +78,16 @@ const clear = () => {
          </div>
          </div>
          <div className="flex justify-center">
+            {!loading?(
+             <>
             <button className="bg-orange-400 hover:bg-orange-600 hover:text-white h-12 w-40 font-bold text-xl mb-5 rounded-lg" onClick={summarizing}>Summarize</button>
             <button className="bg-purple-400 hover:bg-purple-700 hover:text-white h-12 w-40 font-bold text-xl mb-5 ml-4 rounded-lg" onClick={clear}>Clear</button>
+            </>
+            ):(
+               <h1 className="text-3xl text-orange-600 font-bold mb-12">
+                  Loading........Summary !
+               </h1>
+            )}
          </div>
          <div>
             <section className="h-80 bg-green-50">
